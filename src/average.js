2a9863dc7ecd3fc7501fc6c2068d420938a9cbfb
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-08-17 17:08:05 -0300

    Requisito 1 concluído

--- src/average.js
+++ src/average.js
@@ -11,8 +11,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  //adicione seu código aqui
-};
+const average = (array) => {
+  let somaArray = 0; // caso a função receba um valor 0 retorna undefined
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) { // criando o index e retornando undefined para caso a função receba algum valor não numérico
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    somaArray += array[index]; // a variável pega o valor que já tem e acrescenta com o valores contidos no array, conforme o index
+  } return Math.round(somaArray / array.length);
+}; // o math.round foi utilizado para arrendondar o número para o inteiro mais próximo.
 
 module.exports = average;
