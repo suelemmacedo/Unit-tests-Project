6bc26e6e14fd05a28d5be3024e0cc2abde154db6
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-08-18 18:47:06 -0300

    Requisito 6 concluído

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,22 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(Array.isArray (productDetails('firstProduct', 'secondProduct'))).toBe(true);
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('firstProduct', 'secondProduct').length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('firstProduct', 'secondProduct')).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('nome1', 'nome2')).not.toBe('nome1', 'nome2');
     // Teste se os dois productIds terminam com 123.
+    const productsIds1 = productDetails('firstProduct', 'secondProduct')[0].details.productId; // criando constantes para que busque o que há dentro dos productIds dos parâmetros
+    const productsIds2 = productDetails('firstProduct', 'secondProduct')[1].details.productId;
+
+    expect(productsIds1.substring(productsIds1.length - 3)).toBe('123'); // utilizando o substring para captar os caracteres da palavra já capturada na constante acima, fazendo o mesmo abaixo com o productsIds2.
+    expect(productsIds2.substring(productsIds2.length - 3)).toBe('123');
+
   });
 });
